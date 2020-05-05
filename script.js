b48343d1f6d72a1b9fb5e0d5c6a096f2b5ca2a17
w-flowers <wig.flowers@gmail.com>
2020-05-05 16:44:55 +1000

    Test Commit
    - testing

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('button');
+
+btn.onclick = function() {
+  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}
