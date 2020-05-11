927fed99d2af34582d424da4edb2e239c4a7fd5f
w-flowers <wig.flowers@gmail.com>
2020-05-11 21:11:03 +1000

    Added Javascript which enables showing and hiding of elements.
    - Added event handlers to change text colour when the cursor passes over list items
    - Added a click function which changes the text displayed in the main reading area

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const btn = document.querySelector('button');
+let listItems = document.getElementById("proj-list").getElementsByTagName("li");
 
-btn.onclick = function() {
-  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-  document.body.style.backgroundColor = rndCol;
+for(let i = 0; i < listItems.length; i++){
+	listItems[i].addEventListener("mouseover", function (event) {
+	event.target.style.color = "blue";
+	event.target.style.cursor = "pointer";
+	}, false);
+	listItems[i].addEventListener("mouseout", function (event) {
+	event.target.style.color = "black";
+	}, false);
+	listItems[i].addEventListener("click", event => {
+		let heading = document.getElementById("mainhead");
+		let elements = document.getElementsByClassName(listItems[i].className);
+		let newHeading = " ";
+		for(let j = 0; j < elements.length; j++){
+			if (elements[j].classList.contains("proj-heading")) newHeading = elements[j].innerHTML;
+		}
+		heading.innerHTML = newHeading;
+		let descriptions = document.getElementsByClassName("proj-desc");
+		for(let j = 0; j < descriptions.length; j++){
+			if (descriptions[j].classList.contains(listItems[i].className)) descriptions[j].hidden = false;
+			else descriptions[j].hidden = true;
+		}
+	})
 }
